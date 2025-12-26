c808ab05cd7712078b6b81849879dffc0cf04566
AZIZAH <tantesumyatik123@.com>
2025-12-27 06:55:21 +0700

    add splash screen UI

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'LMS App',
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+          ),
+        ),
+      ),
+    );
+  }
+}
